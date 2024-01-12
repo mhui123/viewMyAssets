5263da902c651f7c10efb303a12d10c03c4f2364
mhui123 <mhui7864@gmail.com>
2024-01-12 09:32:52 +0900

    기타수정사항 반영

--- src/main/java/com/myasset/myasset/configuration/WebConfig.java
+++ src/main/java/com/myasset/myasset/configuration/WebConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/* CORS 구성 클래스 */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://api.finance.naver.com").allowedMethods("GET", "POST")
+        registry.addMapping("/**")
+                .allowedOrigins("https://api.finance.naver.com") // 허용할 주소. 모든 주소 허용시 "*"
+                .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
 
--- src/main/java/com/myasset/myasset/vo/SiseVo.java
+++ src/main/java/com/myasset/myasset/vo/SiseVo.java
@@ -1,7 +1,5 @@
 package com.myasset.myasset.vo;
 
-import java.util.Date;
-
 import lombok.Getter;
 import lombok.Setter;
 
